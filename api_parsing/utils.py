692ed09c3b68201823e56be53000b1be293a2509
deorz <TheDamnation1044@yandex.ru>
2022-03-29 23:02:43 +0300

    refactored for heroku

--- api_parsing/utils.py
+++ api_parsing/utils.py
@@ -1,5 +1,4 @@
 import datetime as dt
-import locale
 
 import aiohttp
 
@@ -15,14 +14,11 @@ async def get_json_from_api(url, group_id, begin_date, end_date):
 
 
 def calculate_time(start_time=dt.datetime.now(), delta=0):
-    locale.setlocale(locale.LC_TIME, "ru_RU")
     start_time_str = start_time.strftime('%Y.%m.%d')
-    day_now = start_time.strftime('%a')
     delta = dt.timedelta(days=delta)
     end_time = start_time + delta
     end_time_str = end_time.strftime('%Y.%m.%d')
     return {
         'start_time_str': start_time_str,
         'end_time_str': end_time_str,
-        'day_now': day_now
     }
